bebe1c8ef0e32ac5476befb7c473d71a1d054e59
Kumar Vaibhav <kumar.vaibhav@infinitylearn.com>
2022-07-24 12:07:45 +0530

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'practice_app';
+  name = 'practice_app';
 }
+
